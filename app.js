aafd45730a323943d3e498a9d9a5a19641875d80
Andrew Courey <goldenmambasoft@gmail.com>
2018-09-10 09:11:09 -0400

    Almost there!!!!!!

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-let phrases = [
-    "life is a lie",
-    "flat earth",
-    "triangular universe",
-    "web development",
-    "apple pie"
-];
+let phrases = ["life is a lie", "flat earth", "triangular universe", "web development", "apple pie"];
 var phrase;
 var lives = 5;
 
@@ -12,6 +6,15 @@ function disable(selector) {
     $(selector).prop("disabled", true);
 }
 
+function locationsOfSubstring(substring,string){
+    var str = string;
+    var indices = [];
+    for(var i=0; i<str.length;i++) {
+        if (str[i] === substring) indices.push(i);
+    }
+    return indices;
+  }
+  
 function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -29,45 +32,59 @@ function displayPhrase() {
         $("#phrase ul").append(letter);
     }
 }
-$(".btn__reset").click(() => {
+$(".btn__reset").click(()=>{
     $("#overlay").slideUp(600);
-});
+}
+);
 displayPhrase();
-$("#qwerty").click((event) => {
+$("#qwerty").click((event)=>{
 
     if (event.target.tagName === "BUTTON") {
-        isNotInPhrase = false;
         const key = event.target;
         const letter = key.innerText;
         $(key).addClass("disabled");
         disable(key);
-        for (var i = 0; i < phrase.length; i += 1) {
-            var character = phrase.replace(/\s/g, '').toLowerCase()[i];
-            if (letter.toLowerCase() === character) {
-                $(".letter").eq(i).addClass("show");
-                console.log(`You lost a life [SHOW]  --  isNotInPhrase: ${isNotInPhrase}  letter.toLowerCase() === character: ${letter.toLowerCase() === character} index: ${i} lives: ${lives} character: ${character} letter: ${letter}`);
-                   isNotInPhrase = true;
-            }
-             else 
-            {
-            
-                if (isNotInPhrase === false) {
-                    loseALife();
-                    console.log(`You lost a life [lose a life]  --  isNotInPhrase: ${isNotInPhrase}  letter.toLowerCase() === character: ${letter.toLowerCase() === character} index: ${i} lives: ${lives} character: ${character} letter: ${letter}`);
-                    isNotInPhrase = true;
-                }
-               
-            }
+
+        const matches = locationsOfSubstring(letter,phrase.replace(/\s/g, ''));
+
+        console.log(matches.length);
 
 
+        for (var i = 0; i < matches.length; i += 1) {
+            const match = matches[i];
+            console.log("Match --  " + match);
+
+            $(".letter").eq(match).addClass("show");
+          console.log( $(".letter").eq(match));
+
+
+                // indexs of the matches
+                // go through each index and update th letter accoringly 
+
+// console.log($(".letter").eq(phrase.replace(/\s/g, '').toLowerCase()));
         }
-        }
-    });
+        //     if (phrase.replace(/\s/g, '').toLowerCase().locationsOfSubstring(letter) > -1) {
+        //         $(".letter").eq(phrase.replace(/\s/g, '').toLowerCase().indexOf(letter)).addClass("show");
+
+
+        // }
+        // if (phrase.replace(/\s/g, '').toLowerCase().indexOf(letter) > -1) {
+
+        //     loseALife();
+        // }
+    }
+
+}
+
+);
 
 function loseALife() {
-    if (lives <= 0) {
+    if (lives <= 1) {
+        $("#scoreboard ol li").eq(lives - 1).remove();
+        alert("You lose!")
+
     } else {
         lives -= 1;
-       $("#scoreboard ol li").eq(lives - 1).remove();
+        $("#scoreboard ol li").eq(lives - 1).remove();
     }
 }
